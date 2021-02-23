12af42aee881f992a129688404f242b026d14ad3
fx001122 <64654621+fx001122@users.noreply.github.com>
2021-02-22 20:56:35 -0600

    Update SignIn.py

--- SignIn.py
+++ SignIn.py
@@ -11,7 +11,7 @@ import requests
 
 OpenLuckDraw = False  # 是否开启自动幸运抽奖(首次免费, 第二次5积分/次) 不建议开启 否则会导致多次执行时消耗积分
 Skey = ""  # 酷推 skey
-Cookie = ""  # 抓包Cookie 存在引号时 请使用 \ 转义
+Cookie = "cookieTokenKey=bW9iaWxlOjE3NjkxMzIwMzk2OnlxY1puRG5TfDF8UkNTfDE2MTY2NDA4Mzc4OTh8bVQyaTk0MElVM0k4OGVyNjFhdWtOX3lDR3kwbWh1N2pZbFBSTFgzY2t4MF9lTy5zTDRVNVlwanJMQ3ZzQzYudmFKSnJrWXlLNi5VdXdVaE9tWU5BM0JEeU0zQkxicFRpaHZIeXVRdElTODBBcDMxVnNteURWSG4xNUJVS3Q2SmhOY2tzZHdKQTkuNmdDVC5uVUZnS05VQkhSLkk2aFk2aTlLcWFfQzJxOVprLQ==; cookieContentUserData=eyJwcm92Q29kZSI6IjI5In0=; _uid=176****0396; sajssdk_2015_cross_new_user=1; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22177ccd02a7e3f1-0de6306e727b5a-1d2e3236-259200-177ccd02a7f2dd%22%2C%22first_id%22%3A%22%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%2C%22phoneNumber%22%3A%2217691320396%22%7D%2C%22%24device_id%22%3A%22177ccd02a7e3f1-0de6306e727b5a-1d2e3236-259200-177ccd02a7f2dd%22%7D; pageIndex=1"  # 抓包Cookie 存在引号时 请使用 \ 转义
 Referer = ""  # 抓包referer
 UA = "Mozilla/5.0 (Linux; Android 10; M2007J3SC Build/QKQ1.191222.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36 MCloudApp/7.6.0"
 
